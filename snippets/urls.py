d6ae625d807947736efff5816d3d3ff74c3104f7
lorenzodallamuta <lorenzodallamuta@gmail.com>
2021-06-16 10:24:08 +0200

    switch to hyperlinked relationships

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,7 +5,7 @@ from snippets import views
 urlpatterns = [
     path("", views.api_root),
     path("snippets/", views.SnippetList.as_view(), name="snippet-list"),
-    path("snippets/<int:pk>/", views.SnippetDetail.as_view(), name="snipper-detail"),
+    path("snippets/<int:pk>/", views.SnippetDetail.as_view(), name="snippet-detail"),
     path("snippets/<int:pk>/highlight/",
          views.SnippetHighlight.as_view(), name="snippet-highlight"),
     path("users/", views.UserList.as_view(), name="user-list"),
