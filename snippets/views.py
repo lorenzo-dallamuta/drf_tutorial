b9cde3fdccf4a2a1b25914fcab1753618c559116
lorenzodallamuta <lorenzodallamuta@gmail.com>
2021-06-16 09:39:53 +0200

    wiring users with serializers and views

--- snippets/views.py
+++ snippets/views.py
@@ -1,13 +1,28 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
+from snippets.serializers import UserSerializer
+from account.models import CustomUser as User
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
